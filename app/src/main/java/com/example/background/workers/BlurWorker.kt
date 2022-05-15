a494d34d422d5d683a8beda303fd43be90b89d0a
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-14 23:05:56 -0400

    Created a BlurWorker kotlin file and override the doWork member function since we are using the Worker functionality with the BlurWorker class. Finally implemented the doWork method by returning the value of the try-catch statement.

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,30 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.background.R
+
+class BlurWorker(ctx: Context, params: WorkerParameters): Worker(ctx,params) {
+
+    override fun doWork(): Result {
+        val appContext = applicationContext
+        makeStatusNotification("Blurring Image", appContext)
+
+        return try {
+            val picture = BitmapFactory.decodeResource(
+                appContext.resources,
+                R.drawable.android_cupcake
+            )
+            //blurBitmap(picture, appContext)
+            val outputUri = writeBitmapToFile(appContext, blurBitmap(picture, appContext))
+            makeStatusNotification("Output is $outputUri", appContext)
+            Result.success()
+        } catch (throwable: Throwable){
+            Log.e(TAG, "Error applying blur")
+            Result.failure()
+        }
+    }
+}
